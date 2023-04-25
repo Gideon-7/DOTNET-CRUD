4e5d3c15bb1b469ed41906aa24b5d2b8c6609aec
Gideon-7 <ashimchaudhary7@gmail.com>
2023-04-25 10:57:39 +0530

    Third Commit

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
